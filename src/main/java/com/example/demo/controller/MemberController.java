f4957e27ffdf238a66c9b2f0a23f68682496229f
bingak36 <102848043+bingak36@users.noreply.github.com>
2026-03-12 18:45:40 +0900

    12

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,4 +1,45 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.CreateMemberRequest;
+import com.example.demo.dto.MemberResponse;
+import com.example.demo.dto.UpdateMemberRequest;
+import com.example.demo.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping
+    public MemberResponse create(@RequestBody CreateMemberRequest request){
+        return memberService.create(request);
+    }
+
+
+    @GetMapping
+    public List<MemberResponse> findAll(){
+        return memberService.findAll();
+    }
+
+    @PatchMapping("/{id}")
+    public MemberResponse update(@PathVariable Long id, @RequestBody UpdateMemberRequest request){
+        return memberService.update(id, request);
+    }
+
+    @GetMapping("/{id}")
+    public MemberResponse findById(@PathVariable Long id){
+        return memberService.findById(id);
+    }
+
+    //    삭제
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        memberService.delete(id);
+    }
 }
--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.repository;
 
-public class MemberRepository {
+import com.example.demo.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member ,Long> {
+
+    boolean existsByEmail(String email);
 }
